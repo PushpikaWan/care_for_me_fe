301c07414e5d88ede3e34ebc6db800c1026ba24e
Pushpika Wanniachchi <pushpika@grubtech.com>
2022-10-27 17:44:14 +0530

    added google play link

--- src/components/post_page/post_page.tsx
+++ src/components/post_page/post_page.tsx
@@ -17,9 +17,19 @@ export default function PostPage() {
     }
   }, [])
 
+
+  const renderDownloadButton = () => {
+    return <a className="android-link"
+              href='https://play.google.com/store/apps/details?id=com.care_for_me.care.prod'><img
+        className="android-link-button"
+        alt='android'
+        src="/android-icon.png"/></a>
+  }
+
   return (
       <div className="post-page-main-container">
         {post != null ? <PostCard post={post}/> : null}
+        {renderDownloadButton()}
       </div>
   );
 }
